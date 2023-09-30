b356b94cec195fffbd3df6766bff1afa8a699f77
AdayanASB <andrei.baias5002@gmail.com>
2023-09-30 20:11:26 +0300

    made working audio player, sections, and doted bkg

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,32 @@
-import { languages } from "./language";
+//import { languages } from "./language";
 
-languages.forEach(()=>{
+//languages.forEach(()=>{
+
+//});
+
+let audioPlayer= document.querySelector('.main-audio');
+
+let buttonElementMusic=document.querySelector('.js-button-music');
+let isPlaying=false;
+buttonElementMusic.addEventListener("click",()=>{
+  if(!isPlaying){
+    playAudio();
+    isPlaying=true;
+
+  }
+  else{
+    audioPlayer.pause();
+    audioPlayer.currentTime=0;
+    isPlaying=false;  
+  }
 
 });
+
+function playAudio(){
+  audioPlayer.load();
+  audioPlayer.volume=1;
+  audioPlayer.currentTime=0;
+  audioPlayer.play();
+  audioPlayer.controls=false;
+  console.log('you payed the button');
+}
