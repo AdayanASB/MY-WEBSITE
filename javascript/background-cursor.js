7e197a90ef51533506a5956262ecfc5671b7da35
AdayanASB <andrei.baias5002@gmail.com>
2023-10-21 17:48:38 +0300

    first varient of background text tech page

--- javascript/background-cursor.js
+++ javascript/background-cursor.js
@@ -3,11 +3,24 @@ backgElement=document.querySelector(".background-gradient");
 document.body.onmousemove=(event)=>{
   let userX=event.clientX;
   let userY=event.clientY;
-  console.log(userX);
-  console.log(userY);
+  
   backgElement.animate({
     top:`${userY}px`,
     left:`${userX}px`
   },{duration:3000, fill:'forwards'});
 
+};
+
+textElement=document.querySelector('.random-text-bg');
+textElement.onmousemove=(event)=>{
+  let fullHTML='';
+  console.log("ceva randoamsadm");
+  for(let i=1; i<=256; i++){
+    const radnomNumb=Math.round(Math.random());
+    fullHTML+=`${radnomNumb}`;
+    console.log(radnomNumb);
+    console.log(fullHTML);
+  }
+  event.target.innerText =fullHTML;
+  console.log(fullHTML);
 };
